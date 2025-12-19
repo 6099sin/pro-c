1c65ca0d9cd1287996d45100c1a21ed7d967cfe9
CD <teerapongnunsuwan@gmail.com>
2025-12-19 16:59:35 +0700

    feat: Implement basket and item interaction system with scoring and visual feedback.

--- scripts/objects/Basket.gd
+++ scripts/objects/Basket.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var snap_zone: Area2D = $SnapZone
 @onready var center_point: Marker2D = $CenterPoint
+@onready var sprite: Sprite2D = $Sprite2D
 
 func _ready():
 	snap_zone.body_entered.connect(_on_body_entered)
@@ -21,6 +22,8 @@ func receive_item(item: Item):
 	tween.tween_callback(func(): process_item(item))
 
 func process_item(item: Item):
+	play_hit_effect(item.type)
+	
 	if item.type == Utils.ItemType.FRUIT:
 		GameManager.add_score(10)
 		SignalBus.request_sfx.emit("pop")
@@ -31,3 +34,15 @@ func process_item(item: Item):
 		# GameManager.end_game() # if bombs are fatal
 	
 	item.deactivate()
+
+func play_hit_effect(type: Utils.ItemType):
+	var tween = create_tween().set_parallel(true)
+	
+	# Stretch / Squash
+	sprite.scale = Vector2(1.2, 0.8)
+	tween.tween_property(sprite, "scale", Vector2.ONE, 0.3).set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_OUT)
+	
+	# Color Flash
+	var flash_color = Color(0.5, 1.5, 0.5) if type == Utils.ItemType.FRUIT else Color(1.5, 0.5, 0.5)
+	sprite.modulate = flash_color
+	tween.tween_property(sprite, "modulate", Color.WHITE, 0.3)
