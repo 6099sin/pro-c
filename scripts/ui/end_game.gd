340acfa2e3dabad6ebba6d61043210593ce47a31
CD <teerapongnunsuwan@gmail.com>
2026-01-12 13:16:42 +0700

    add node UI in Score Scene

--- scripts/ui/end_game.gd
+++ scripts/ui/end_game.gd
@@ -1,12 +1,16 @@
 extends Control
 
-@onready var label_alpha: Label = $CenterContainer/VBoxContainer/LabelAlpha
-@onready var label_beta: Label = $CenterContainer/VBoxContainer/LabelBeta
-@onready var label_sum: Label = $CenterContainer/VBoxContainer/LabelSum
+@onready var label_alpha: Label = $VBoxContainer/PanelContainer/MarginContainer/VBoxContainer/MarginContainer2/PanelContainer3/MarginContainer2/addPointAlpha
+@onready var label_beta: Label = $VBoxContainer/PanelContainer/MarginContainer/VBoxContainer/MarginContainer3/PanelContainer2/MarginContainer2/addPointBeta
+@onready var label_sum: Label = $VBoxContainer/MarginContainer3/HBoxContainer/PanelContainer2/MarginContainer2/allPoint
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	label_alpha.text = "Alpha Score: %d" % GameManager.score_alpha
-	label_beta.text = "Beta Score: %d" % GameManager.score_beta
-	label_sum.text = "Total Score: %d" % GameManager.score
+	# label_alpha.text = "Alpha Score: %d" % GameManager.score_alpha
+	# label_beta.text = "Beta Score: %d" % GameManager.score_beta
+	# label_sum.text = "Total Score: %d" % GameManager.score
+
+	label_alpha.text = "%d" % GameManager.score_alpha
+	label_beta.text = "%d" % GameManager.score_beta
+	label_sum.text = "%d" % GameManager.score
