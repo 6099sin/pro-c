783c41c079987442224c1545dc6e594e688b0b8f
CD <teerapongnunsuwan@gmail.com>
2026-01-16 12:48:02 +0700

    export game build

--- export/web/index.service.worker.js
+++ export/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1768539852|1307900248';
+const CACHE_VERSION = '1768542400|766289728';
 /** @type {string} */
 const CACHE_PREFIX = 'Pro-C-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
